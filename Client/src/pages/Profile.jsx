aecae1b88973b79d863f62b55439858a2e3b59d3
Abdullah Javaid <akhaan686@gmail.com>
2024-10-13 11:10:41 +0500

    complete delete user Listing Functionality

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -138,7 +138,22 @@ function Profile() {
       setShowListingError(error);
     }
   };
+const handleListingDelete = async (listingId) =>{
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method : "DELETE",
+    });
+    const data = await res.json();
+    if(data.success ===false){
+        console.log(data.message)
+        return;
+    }
+    setUserListings((prev)=> prev.filter((listing)=> listing._id !==listingId) );
+  } catch (error) {
+    console.log(error.message)
+  }
 
+}
   return (
     <div className="p-3  max-w-lg mx-auto  ">
       <h1 className="text-3xl font-bold text-center py-7">Profile</h1>
@@ -248,7 +263,7 @@ function Profile() {
               <p>{listing.name}</p>
             </Link>
             <div className="flex flex-col items-center">
-              <button className="text-red-700 uppercase">Delete</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
               <button className="text-green-700 uppercase">Edit</button>
             </div>
           </div>
--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,14 +14,15 @@ try {
  export const deleteListing = async(req , res , next) =>{
   const listing = await Listing.findById(req.params.id);
   if(!listing){
-    return next(errorHandler(404 , "Listing not Found"))
-  }
+    return next(errorHandler(404 , "Listing Not Found"))
+  };
   if(req.user.id !== listing.userRef){
-    return next(errorHandler(401, "You can Only Delete your OWn Listing "))
-    res.status(200).json("Listing Deleted Successfully")
-  }
+    return next(errorHandler(401 , "You can only delete your own listings"))
+  };
+   
   try {
     await Listing.findByIdAndDelete(req.params.id)
+    res.status(200).json("Listing Deleted Successfully  ")
   } catch (error) {
     next(error)
   }
